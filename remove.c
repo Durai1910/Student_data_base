86f229799b0f721cc320f54086fa42ab95de9ed5
Durai1910 <162876861+Durai1910@users.noreply.github.com>
2025-11-10 19:44:21 +0530

    Update remove.c

--- remove.c
+++ remove.c
@@ -21,29 +21,9 @@ void remove_node()
 	if((op=='R')||(op=='r'))
 	{
 		int n;
-		printf("\n\tEnter which Roll_no's Data to remove....");
-		scanf(" %d",&n);
-		printf("\n\n");
-		while(ptr!=0)
-		{
-			if(ptr->roll==n)
-			{
-				if(ptr->prev==0)
-					ptr->next->prev = ptr->prev;
-				else if(ptr->next==0)
-					ptr->prev->next = ptr->next;
-				else
-				{
-					ptr->prev->next = ptr->next;
-					ptr->next->prev = ptr->prev;
-				}
-				free(ptr);
-				printf("\tThe given Roll_no's Data is deleted...\n");
-				return;
-			}
-			else
-				ptr=ptr->next;
-		}
+		roll_no();
+		printf("\tThe given Roll_no's Data is deleted...\n");
+		return;
 	}
 	else if((op=='N')||(op=='n'))
 	{
@@ -55,7 +35,29 @@ void remove_node()
 		{
 			if((strcmp(ptr->name,s))==0)
 			{
-				if(ptr->prev==0)
+				count++;
+				temp=ptr;
+			}
+			ptr=ptr->next;
+		}ptr=hptr;
+		if(count>1)
+		{
+			printf("\n\tThe given name occurs more than once in the list...");
+			while(ptr!=0)
+			{
+				if(strcmp(ptr->name,s)==0)
+				{
+					printf("\n\t%d\t%[^\n]\t%f",ptr->roll,ptr->name,ptr->mark);
+				}
+				ptr=ptr->next;
+			}
+			roll_no();
+			printf("\tThe given Name's Data is deleted...\n");
+			return;
+		}
+		else
+		{
+			if(ptr->prev==0)
 					ptr->next->prev = ptr->prev;
 				else if(ptr->next==0)
 					ptr->prev->next = ptr->next;
@@ -67,9 +69,6 @@ void remove_node()
 				free(ptr);
 				printf("\tThe given Name's Data is deleted...\n");
 				return;
-			}
-			else
-				ptr=ptr->next;
 		}
 	}
 	else
@@ -78,6 +77,31 @@ void remove_node()
 		return;
 	}
 	printf("\tThe given data is not in the list.....\n");
+}
+void roll_no()
+{
+	printf("\n\tEnter which Roll_no's Data to remove....");
+	scanf(" %d",&n);
+	printf("\n\n");
+	while(ptr!=0)
+	{
+		if(ptr->roll==n)
+		{
+			if(ptr->prev==0)
+				ptr->next->prev = ptr->prev;
+			else if(ptr->next==0)
+				ptr->prev->next = ptr->next;
+			else
+			{
+				ptr->prev->next = ptr->next;
+				ptr->next->prev = ptr->prev;
+			}
+			free(ptr);
+			break;
+		}
+		else
+			ptr=ptr->next;
+	}
 }
 			
 
